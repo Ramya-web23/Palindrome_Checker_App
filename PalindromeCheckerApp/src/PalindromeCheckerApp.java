31aaaf2ece74e46fbd01ca5ffaad0497fb5b43e7
ShajhinRoe <sr6922@srmist.edu.in>
2026-02-16 15:38:02 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeCheckerApp/src/PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+/*
+1.welcome message
+2.version
+ */
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
